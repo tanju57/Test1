3c617bbc8be86812e74c256c0c524b5672568b7c
unknown <02230080.cst@rub.edu.bt>
2024-05-25 04:05:05 +0600

    simple operation

---
+++ simpleoperation.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+
+print(f"Hello {name}\n")
+
+print(f"Sum is:  {addition_result}")
+print(f"Difference is:  {subtraction_result}")
+print(f"Product:  {multiplication_result}")
+print(f"Quotient is:  {division_result}")
